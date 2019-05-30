c14a22c469ac294ce8910f737d313daa6c0dbd32
juanse1080 <juanmarcon1080@gmail.com>
2019-05-30 18:02:11 -0500

    kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,6 +59,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'admin' => \Illuminate\Routing\Middleware\RoleMiddleware::class,
+        'admin' => \App\Http\Middleware\RoleMiddleware::class,
     ];
 }
