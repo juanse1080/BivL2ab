5769d3964decfe2a5dfc7ad29c07ae5e2bd7c572
juanse1080 <juanmarcon1080@gmail.com>
2019-06-10 03:00:55 -0500

    list new

--- resources/views/news/listNews.blade.php
+++ resources/views/news/listNews.blade.php
@@ -16,17 +16,18 @@
 
 <?php 
     $card_deck = '</div><div class="card-deck">';
-    function edit_button($id) {
-        return '<a href='.route("news.edit", $id).' role="button" class="btn btn-success">Edit</a>';
-    } 
 ?>
 
 <div class="container">
-        <h1 class="display-3 text-muted">News</h1>
+    <div class="row mb-3">
+        <div class="col-12">
+            <h5 class="text-muted mb-3">News:</h5>
+        </div>
+    </div>
     <div class="card-deck">
     @foreach ($news as $key => $new)
             <!--Card-->
-            <div class="card card-cascade">
+            <div class="card card-cascade" onclick="location.href='{{route('news.show', $new->pk_new)}}'">
 
                 <!--Card image-->
                 <div class="view view-cascade">
@@ -40,18 +41,15 @@
                 <!--Card content-->
                 <div class="card-body card-body-cascade">
                     <!--Title-->
-                    <h4 class="card-title text-center"><strong>{{$new->title}}</strong></h4>
-                    <h5 class="text-center">{{ strftime("%B %d, %Y",strtotime($new->created_at)) }}</h5>
+                    <p class="mb-3 text-center"><strong>{{$new->title}}</strong></p>
+                    {{-- <h5 class="text-center">{{ strftime("%B %d, %Y",strtotime($new->created_at)) }}</h5> --}}
                     <!--Facebook-->
                 </div>
                 <!--/.Card content-->
-                <div class="card-footer text-center">
-                    <a href="{{route('news.show', $new->pk_new)}}" role="button" class="btn btn-secondary">View</a>
-                    @if (Auth::check())
-                        @if (session('role') == 0 || session('role') == 1)
-                            {!! edit_button($new->pk_new) !!}
-                        @endif
-                    @endif
+                <div class="card-footer">
+                    <div class="float-right text-muted">
+                        {{ strftime("%B %d, %Y",strtotime($new->created_at)) }}
+                    </div>
                 </div>
             </div>
             <!--/.Card-->
--- resources/views/news/viewNew.blade.php
+++ resources/views/news/viewNew.blade.php
@@ -4,25 +4,32 @@
 <div class="container">
     <div class="row mb-3">
         <div class="col-12">
-            <h3 class="text-muted mb-3 text-center" style="font-size: 2rem;">{{$new->title}}</h3>
+            <h5 class="text-muted mb-3 text-center">{{$new->title}}</h5>
         </div>
     </div>
     <div class="row justify-content-center mb-5">
-        <div class="col-10 text-center">
-            <img src="{{asset($new->photo)}}" style="width:36%; height:250px;" class="rounded" alt="{{$new->title}}">
+        <div class="col-12 text-center">
+            <img src="{{asset($new->photo)}}" style="max-height:400px;object-fit:scale-down" class="rounded" alt="{{$new->title}}">
         </div>
+        
     </div>
     <div class="row justify-content-center">
-        <div class="col-10">
+        <div class="col-sm-10">
             <p ALIGN="justify">{!! nl2br(e($new->description)) !!}</p>
-            <a class="badge badge-pill badge-danger text-white" onclick="$('#{{$new->pk_new}}').submit()">
-                <i class="fas fa-trash"></i>
-                Delete
-            </a>
-            <form action="{{route('news.destroy', $new->pk_new)}}" id="{{$new->pk_new}}" method="POST">
-                @csrf
-                @method('DELETE')
-            </form>
+            <div class="float-right">
+                <a href="{{route('news.edit', $new->pk_new)}}" class="badge badge-pill badge-primary">
+                    <i class="fas fa-edit"></i>
+                    Edit
+                </a>
+                <a class="badge badge-pill badge-danger text-white" onclick="$('#{{$new->pk_new}}').submit()">
+                    <i class="fas fa-trash"></i>
+                    Delete
+                </a>
+                <form action="{{route('news.destroy', $new->pk_new)}}" id="{{$new->pk_new}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                </form>
+            </div>
         </div>
     </div>
 </div>
