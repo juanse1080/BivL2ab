3e2f215a07172e691d5f699660f37e39e5298ea2
Santiago Gomez <santiagogomez.h@outlook.com>
2019-09-06 01:59:16 -0500

    fix

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -15,7 +15,7 @@ class CourseController extends Controller
     }
     
     public function index() {
-        $courses = Course::orderBy('name', 'ASC');
+        $courses = Course::orderBy('name', 'ASC')->get();
         return view('courses.listCourses', compact('courses'));
     }
 
